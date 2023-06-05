24f4279645158299f07e835707e06f6e68662595
Badis Beloucif <beloucifbadis@gmail.com>
2023-06-05 15:33:21 +0200

    creation components Navbar

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-dom";
+
+export default function NavBar() {
+  return (
+    <header>
+      <a href="/">
+        <img
+          src="/assets/logo/logo-AFAC.png"
+          alt="logo AFAC"
+          width="350"
+          height="60"
+          className="logo-AFAC"
+        />
+      </a>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/acceuil">Acceuil</Link>
+          </li>
+          <li>
+            <Link to="/galerie">Galerie</Link>
+          </li>
+          <li>
+            <Link to="/oeuvres">Oeuvres</Link>
+          </li>
+          <li>
+            <Link to="/auteurs">Auteurs</Link>
+          </li>
+          <li>
+            <Link to="/aPropos">A Propos</Link>
+          </li>
+          <li>
+            <Link to="/connexion">Connexion</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
