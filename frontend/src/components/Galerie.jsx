5c665769d6dc81e5cb5a6f91c5276f0f3f5c7fc5
Enzo Castagnos <castagnos.e@gmail.com>
2023-06-06 15:13:47 +0200

    implemantation animation hover sur images galerie

--- frontend/src/components/Galerie.jsx
+++ frontend/src/components/Galerie.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import imagesData from "./images.json"; // Import the JSON file
+import imagesData from "./images.json"; // Importer le fichier JSON
+import "./Galerie.css";
 
 function Gallery() {
   const [images, setImages] = useState([]);
@@ -11,16 +12,27 @@ function Gallery() {
   }, []);
 
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {images.map((image) => (
-        <div key={image.id} className="bg-gray-200 h-48">
-          <img
-            src={image.url}
-            alt="Aquarelle"
-            className="object-cover h-full w-full"
-          />
-        </div>
-      ))}
+    <div className="bg-primary_blue flex justify-center">
+      <div className="grid grid-cols-2 gap-12">
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className="bg-gray-600 h-64 w-[500px] opacity-100 transition-opacity ease-in-out duration-300 relative"
+          >
+            <img
+              src={image.url}
+              alt="Aquarelle"
+              className="object-cover h-full w-full opacity-100 hover:opacity-30"
+            />
+            <div className="texthover opacity-0 hover:opacity-100 transition-opacity ease-in-out duration-300 absolute inset-0 flex items-center justify-center">
+              <div className="text grey-600">
+                <p>Titre oeuvre</p>
+                <p>Autre texte</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
