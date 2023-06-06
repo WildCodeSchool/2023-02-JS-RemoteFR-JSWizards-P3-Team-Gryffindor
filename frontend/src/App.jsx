a4994d1763bf5c74c78b34443fdd7110ef231bac
Pierre Lacoste <113516189+PierreLacoste@users.noreply.github.com>
2023-06-06 10:01:04 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,10 @@ import "./App.css";
 // import Biographie from "./components/Biographie";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+    </div>
+  );
 }
 
 export default App;
