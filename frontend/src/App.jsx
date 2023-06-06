f1c9f30686cb28cc07a2b0a3341279891658dc55
Pierre Lacoste <p.lacoste33000@gmail.com>
2023-06-06 10:46:19 +0200

    debut apropos page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import APropos from "./components/APropos";
 // import Biographie from "./components/Biographie";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <APropos />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/APropos.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function APropos() {
+  return (
+    <div className="bg-primary_blue min-h-screen">
+      <div>
+        <img src="./src/assets/Afac.png" alt="Logo AFAC" />
+      </div>
+      <div>
+        <p>
+          Association des Familles amies de CAPELINE- Centre D'Accompagnement
+          Pédagogique en Ligne et d'Ingénierie pédagogique. <br />
+          <br /> Soutien aux élèves, accompagnement et/ou formation d'étudiants,
+          d'enseignants ou de parents. <br /> CAPELINE 974 est un projet de
+          plate-forme d'informations, et de formations à caractère pédagogiques,
+          relatives aux outre-mers. C'est aussi une mise en relation de
+          partenaires divers, impliqués dans le développement durable des
+          outre-mers, en particulier dans l'océan Indien. <br /> Elle s'adresse
+          aux élèves de la zone et à leurs enseignants, à leurs familles, et aux
+          collectivités. Ses publications pédagogiques ou scientifiques sont
+          vérifiées par un Comité scientifique. <br /> Ses actions en ligne et
+          sur le terrain s'appuient sur des contributions bénévoles, mais aussi
+          sur le travail de stagiaires rémunérés, et, pour 2021, d'un emploi
+          aidé (stagiaire en formation à l'IUT de Saint-Pierre). Des
+          contributions occasionnelles de chercheurs ou de spécialistes dans le
+          domaine du numérique et/ou de l'enseignement nourrissent la réflexion
+          autour de l'enseignement avec le numérique (Réunion, Inde, Madagascar,
+          Belgique...)
+        </p>
+      </div>
+      <div>
+        <img src="./src/assets/image_icono.svg" alt="Iconotheque" />
+      </div>
+    </div>
+  );
+}
+
+export default APropos;
