34b98527ae5e62212f1794ac5340cec52603d2d8
Badis Beloucif <beloucifbadis@gmail.com>
2023-06-06 10:33:30 +0200

    Navbar terminé

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
+// import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Components/NavBar";
-
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
       <NavBar />
+      <Home />
+
       <p>coucou</p>
     </div>
   );
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";//
 import "./Navbar.css";
 
 export default function NavBar() {
   return (
+    /*
     <header>
       <a href="/">
         <img
@@ -13,7 +14,7 @@ export default function NavBar() {
           className="logo-AFAC"
         />
       </a>
-      <nav>
+      <nav> 
         <ul>
           <li>
             <Link to="/acceuil">Acceuil</Link>
@@ -32,6 +33,39 @@ export default function NavBar() {
           </li>
           <li>
             <Link to="/connexion">Connexion</Link>
+          </li>
+        </ul>
+      </nav>
+    </header> */
+    <header className="flex bg-primary_black">
+      <a href="/acceuil">
+        <img
+          src="./src/assets/logo/logo-AFAC.png"
+          alt="logo AFAC"
+          width="100"
+          height="50"
+          className="logo-AFAC"
+        />
+      </a>
+      <nav>
+        <ul className="container">
+          <li>
+            <a href="acceuil">Acceuil</a>
+          </li>
+          <li>
+            <a href="galerie">Galerie</a>
+          </li>
+          <li>
+            <a href="oeuvres">Oeuvres</a>
+          </li>
+          <li>
+            <a href="auteurs">Auteurs</a>
+          </li>
+          <li>
+            <a href="aPropos">A Propos</a>
+          </li>
+          <li>
+            <a href="connexion">Connexion</a>
           </li>
         </ul>
       </nav>
