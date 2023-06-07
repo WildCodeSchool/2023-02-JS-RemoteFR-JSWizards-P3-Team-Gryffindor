ab2537d948f8fdfdd84f1ded31d5569a67e92e32
Enzo Castagnos <castagnos.e@gmail.com>
2023-06-07 12:04:35 +0200

    creation de la page oeuvre vitrine pour permettre la creation de la route et proposer un visuel pour presentation site

--- frontend/src/Components/OeuvreSolo.jsx
+++ frontend/src/Components/OeuvreSolo.jsx
@@ -1,7 +1,23 @@
 export default function OeuvreSolo() {
   return (
     <div>
-      <h1>Salut la compagnie</h1>
+      <div className="flex">
+        <h1 className="text-white ml-[164px] mt-5">
+          Titre de l'oeuvre, année de création
+        </h1>
+      </div>
+      <div className="flex flex-col items-center">
+        <img
+          className="mt-2 h-[600px]"
+          src="./src/assets/images/thin-rectangle.jpg"
+          alt="Description de l'oeuvre"
+        />
+      </div>
+      <div className="flex">
+        <h2 className="text-white ml-[164px] mt-5">
+          Nom de l'auteur, type d'oeuvre - Couleur : Dim x Dim
+        </h2>
+      </div>
     </div>
   );
 }
