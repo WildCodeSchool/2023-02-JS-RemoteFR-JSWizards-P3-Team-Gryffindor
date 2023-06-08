d0efda3d91aaa6496598ee238780188aefdb5a42
Kyxk <kylian.gronnier@gmail.com>
2023-06-08 14:45:44 +0200

    accueil-carrousel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavBar from "./Components/Navbar";
+import NavBar from "./components/Navbar";
 import "./App.css";
 
-import Gallery from "./Components/Galerie";
+import Gallery from "./components/Galerie";
 import AuteurPage from "./pages/AuteurPage";
 import AProposPage from "./pages/AProposPage";
-import Connexion from "./Components/Connexion";
-import Accueil from "./Components/Accueil";
-import OeuvreSolo from "./Components/OeuvreSolo";
+import Connexion from "./components/Connexion";
+import Accueil from "./components/Accueil";
+import OeuvreSolo from "./components/OeuvreSolo";
 // import Utilisateur from "./Components/Utilisateur";
-
-
-
-
-
 
 function App() {
   return (
-
     <Router>
       <div className="App">
         <NavBar />
-       
 
         <Routes>
           <Route path="/accueil" element={<Accueil />} />
