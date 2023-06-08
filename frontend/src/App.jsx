4219a1ccd4b642cde466e0749781f9cc3d0134ac
Enzo Castagnos <castagnos.e@gmail.com>
2023-06-08 18:16:51 +0200

    rajout des images fournies par le client dans assets + creation API avec elements fournis par client + integration et affichage des images sur la page galerie depuis API locale

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,38 +4,30 @@ import "./App.css";
 // import Utilisateur from "./Components/Utilisateur";
 // import Gallery from "./Components/Galerie";
 
-
 import Gallery from "./Components/Galerie";
 import AuteurPage from "./pages/AuteurPage";
 import AProposPage from "./pages/AProposPage";
 import Connexion from "./Components/Connexion";
 
-
-
-
-import Accueil from "./components/Accueil";
-
-
-
-
+import Accueil from "./Components/Accueil";
 
 function App() {
   return (
-
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Accueil />
-        <Routes>
-          <Route path="/galerie" element={<Gallery />} />
-          <Route path="/auteur" element={<AuteurPage />} />
-          <Route path="/aPropos" element={<AProposPage />} />
-          <Route path="/connexion" element={<Connexion />} />
-        </Routes>
-      </div>
-    </Router>
-
-   
+    <div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Accueil />
+          <Routes>
+            <Route path="/galerie" element={<Gallery />} />
+            <Route path="/auteur" element={<AuteurPage />} />
+            <Route path="/aPropos" element={<AProposPage />} />
+            <Route path="/connexion" element={<Connexion />} />
+          </Routes>
+        </div>
+      </Router>
+      <Gallery />
+    </div>
   );
 }
 
