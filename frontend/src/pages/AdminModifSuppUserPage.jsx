7215132a210bf5d94cbe82994789cdc3fb34c12a
Pierre Lacoste <p.lacoste33000@gmail.com>
2023-06-09 11:46:39 +0200

    Composant Gerer les utilisateurs

--- frontend/src/pages/AdminModifSuppUserPage.jsx
+++ frontend/src/pages/AdminModifSuppUserPage.jsx
@@ -1,11 +1,17 @@
 import AdminGerer from "../Components/AdminGerer";
 import AdminModifUser from "../Components/AdminModifUser";
+import AdminBar from "../Components/AdminBar";
 
 function AdminModifSuppUserPage() {
   return (
-    <div>
-      <AdminGerer />
-      <AdminModifUser />
+    <div className="">
+      <div className="flex w-2/6">
+        <AdminBar />
+        <div>
+          <AdminGerer />
+          <AdminModifUser />
+        </div>
+      </div>
     </div>
   );
 }
