cc6fec166c245fa4dcbae4fc43b820e9c4c0c8e0
Enzo Castagnos <castagnos.e@gmail.com>
2023-06-23 14:31:48 +0200

    Modification affichage des oeuvres + creation du lien navigate en page galerie et page oeuvre + rajout des titres raccourci dans json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import ConnexInscri from "./pages/ConnexInscriPage";
 import AProposPage from "./pages/AProposPage";
 import Accueil from "./Components/Accueil";
 import OeuvreSolo from "./Components/OeuvreSolo";
-// import Utilisateur from "./Components/Utilisateur";
+import imagesData from "./Components/Oeuvres.json"; // Importez le fichier JSON d'œuvres
 
 function App() {
   return (
@@ -18,8 +18,12 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Accueil />} />
-          <Route path="/galerie" element={<Gallery />} />
-          <Route path="/oeuvres" element={<OeuvreSolo />} />
+          <Route path="/galerie/*" element={<Gallery />} />
+          <Route
+            path="/oeuvres/:id"
+            element={<OeuvreSolo oeuvres={imagesData} />} // Passez les données d'œuvre à la page OeuvreSolo
+          />
+
           <Route path="/auteur" element={<AuteurPage />} />
           <Route path="/aPropos" element={<AProposPage />} />
           <Route path="/connexion" element={<ConnexInscri />} />
